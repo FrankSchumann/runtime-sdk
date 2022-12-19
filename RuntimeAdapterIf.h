52156d288175834b624a729bac1c8ca2b1bc3c21
frank <frank@Debian.myguest.virtualbox.org>
2022-12-19 18:00:20 +0100

    add public

--- RuntimeAdapterIf.h
+++ RuntimeAdapterIf.h
@@ -4,6 +4,7 @@
 
 class RuntimeAdapterIf : public COPA::ComponentIf
 {
+   public:
     virtual void startApplications() = 0;
     virtual void stopApplications() = 0;
     virtual void subscribe() = 0;
