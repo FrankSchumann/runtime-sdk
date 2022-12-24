c83da478238661f323f8ad88b8051e0074d2fef9
frank <frank@Debian.myguest.virtualbox.org>
2022-12-24 14:03:29 +0100

    add subscribe to RuntimeIf

--- RuntimeIf.h
+++ RuntimeIf.h
@@ -6,6 +6,7 @@
 
 class RuntimeIf : public COPA::ComponentIf
 {
+	virtual void subscribe() = 0;
     virtual void startApplications() = 0;
     virtual void stopApplications() = 0;
     virtual std::string getRuntimeVersion() = 0;
