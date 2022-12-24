54274788a10bb2a17ac0d3663df0b3afed6ab7a1
frank <frank@Debian.myguest.virtualbox.org>
2022-12-24 11:27:34 +0100

    add parameter to subscribe, add variable runtimes, add method get

--- RuntimeAdapterIf.h
+++ RuntimeAdapterIf.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <string>
+
 #include "copa-pdk/component/ComponentIf.h"
+#include "runtime-sdk/RuntimeIf.h"
 
 class RuntimeAdapterIf : public COPA::ComponentIf
 {
    public:
     virtual void startApplications() = 0;
     virtual void stopApplications() = 0;
-    virtual void subscribe() = 0;
+    virtual void subscribe( std::string const& name, std::shared_ptr< RuntimeIf > const runtime ) = 0;
+    virtual std::shared_ptr< RuntimeIf > get( std::string const& name ) const = 0;
 };
